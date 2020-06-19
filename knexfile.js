dcb17b7678db8cade5c01048010a6442dcb418b8
Your Name <you@example.com>
2020-06-19 10:33:30 -0400

    adds pg driver

--- knexfile.js
+++ knexfile.js
@@ -1,45 +1,46 @@
 const pgConnection = process.env.DATABASE_URL || "postgresql://postgres@localhost/hobbits";
 
 module.exports = {
-  development: {
-    client: "sqlite3",
-    connection: {
-      filename: "./data/hobbits.db3",
+    development: {
+        client: "sqlite3",
+        connection: {
+            filename: "./data/hobbits.db3",
+        },
+        useNullAsDefault: true,
+        migrations: {
+            directory: "./data/migrations",
+        },
+        seeds: {
+            directory: "./data/seeds",
+        },
     },
-    useNullAsDefault: true,
-    migrations: {
-      directory: "./data/migrations",
-    },
-    seeds: {
-      directory: "./data/seeds",
-    },
-  },
-  testing: {
-    client: "sqlite3",
-    connection: {
-      filename: "./data/test.db3",
-    },
-    useNullAsDefault: true,
-    migrations: {
-      directory: "./data/migrations",
-    },
-    seeds: {
-      directory: "./data/seeds",
-    },
-  },
 
-  production: {
-    client: "pg",
-    connection: pgConnection,
-    pool: {
-      min: 2,
-      max: 10,
+    testing: {
+        client: "sqlite3",
+        connection: {
+            filename: "./data/test.db3",
+        },
+        useNullAsDefault: true,
+        migrations: {
+            directory: "./data/migrations",
+        },
+        seeds: {
+            directory: "./data/seeds",
+        },
     },
-    migrations: {
-      directory: "./data/migrations",
+
+    production: {
+        client: "pg",
+        connection: pgConnection,
+        pool: {
+            min: 2,
+            max: 10,
+        },
+        migrations: {
+            directory: "./data/migrations",
+        },
+        seeds: {
+            directory: "./data/seeds",
+        },
     },
-    seeds: {
-      directory: "./data/seeds",
-    },
-  },
 };
